104816a1895e5c158eed88d3c7bd3a5e5a5571c4
ToothBrush <ang.gi.mo.cci1234@gmail.com>
2025-05-16 17:23:48 +0900

    좌클 미감지

--- src/main/kotlin/io/github/haburashi76/enchantx/eventListeners/AnvilListener.kt
+++ src/main/kotlin/io/github/haburashi76/enchantx/eventListeners/AnvilListener.kt
@@ -14,6 +14,7 @@ import org.bukkit.entity.HumanEntity
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
 import org.bukkit.event.inventory.ClickType
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.inventory.ItemStack
@@ -31,7 +32,7 @@ class AnvilListener(
 
     @EventHandler
     fun onClick(event: PlayerInteractEvent) {
-        if (event.clickedBlock?.type == Material.ANVIL && !event.player.isSneaking) {
+        if (event.clickedBlock?.type == Material.ANVIL && !event.player.isSneaking && event.action == Action.RIGHT_CLICK_BLOCK) {
             event.isCancelled = true
             anvilSelectInterface(event.player)
         }
--- src/main/kotlin/io/github/haburashi76/enchantx/eventListeners/SmithingListener.kt
+++ src/main/kotlin/io/github/haburashi76/enchantx/eventListeners/SmithingListener.kt
@@ -14,6 +14,7 @@ import org.bukkit.entity.HumanEntity
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
 import org.bukkit.event.inventory.ClickType
 import org.bukkit.event.inventory.PrepareSmithingEvent
 import org.bukkit.event.player.PlayerInteractEvent
@@ -55,7 +56,7 @@ class SmithingListener(
 
     @EventHandler
     fun onClick(event: PlayerInteractEvent) {
-        if (event.clickedBlock?.type == Material.SMITHING_TABLE && !event.player.isSneaking) {
+        if (event.clickedBlock?.type == Material.SMITHING_TABLE && !event.player.isSneaking && event.action == Action.RIGHT_CLICK_BLOCK) {
             event.isCancelled = true
             smithingSelectInterface(event.player)
         }
